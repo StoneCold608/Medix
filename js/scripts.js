55f08ce482aaa322ba75488bbbf0ba4ed5c9a38f
EugeneMaz10 <46795401+EugeneMaz10@users.noreply.github.com>
2021-01-19 12:06:32 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,85 +1,165 @@
-
- <script type="text/javascript">
-	 // business logic
-         // business logic
-		 
-        //Javascript Object
-		 //var dicloxacilin
-	//{
-	//	name:dicloxacilin
-	//	price:2800
-	//	type: capsule
-	//	delivery:home
-	//}
-		 
-        //dicloxacilin.name
-	var result = document.getElementById("result-integumentary")
+<script>
+var OrderNew = [];
 	
-        var addcartbtn  = document.getElementById("add-cart")
-        addcartbtn.addEventListener("click",function()
-	{ 
-		var med = [{name:dicloxacilin price:2800 type: capsule delivery:home},{name:dicloxacilin price:2300 type:syrup delivery:home}]
-		render-output(med)
-	
-	});
-		 
-	//JSON(Javascript Object Notaion)
-	       
-		 
-		 function render-output(data)
-		 {
-			 var htmlString = "";
-			 for (i=0;i<data.length;i++)
-			 {
-				 htmlString+="<p>"+ data[i].name +"."+"</p>"
-			 }
-			 result.insertAdjacentHTML("before-end",htmlString);
-		 }
-	 
+function Med(medName,medType,medStrength,medDelivery, medQuantity) {
+    this.medName=medName;
+    this.medType= medType;
+    this.medStrength=medStrength;
+    this.medDelivery=medDelivery;
+    this.medQuantity=medQuantity;
+}
+var medPrice= [900,600,400,]
+Med.prototype.totalPrice = function(){
+    return (this. medStrength+ this.medDelivery + this.) * this.medQuantity
+};
+Med.prototype.medN = function () {
+    if (this.medType == 900) {
+        return "Panadol"
         
-		 
-	$('#med-selector option').each(function()
-	{
-		
-		sum += Number($(this).val());		
-		optionValues.push($(this).val());
-	});
-	
-		$('#result').html(sum);
-		$( ".med-selector" ).val();
-		
-var add = function(number1, number2) 
-{
-return number1 + number2;
-};
-var subtract = function(number1, number2)
-{
-return number1 - number2;
-};
-var multiply = function(number1, number2)
-{
-return number1 * number2;
-};
-var divide = function(number1, number2)
-{
-return number1 / number2;
-};
+    } else if (this.medType == 600) {
+        return "Strepsils"
+    } else if (this.medType == 400) {
+        return "Brufen"
+    }
+}
+Med.prototype.medS = function () {
+    if (this.medStrength == 150) {
+        return "Syrup"
+        
+    } else if (this.medStrength == 200) {
+        return "Capsule"
+    } else if (this.medStrength == 250) {
+        return "Injection"
+    }
+}
+Med.prototype.medD =function(){
+    if (this.medDelivery == 150){
+        return "Main Branch Pickup"
+    }
+    else if (this.medDelivery == 170){
+        return " Sub Branch Pickup"
+    }
+    else if ( this.medDelivery == 200){
+        return"Home Delivery"
+    }
+}
 
-$(document).ready(function()
- {
-$("form#add").submit(function(event)
- {
+$("#checkout").click(function (event) {
+    
+    var MedName = $("#Name").val();
+    
+    var MedType = parseInt( $("#Type").val());
+    
+    var MedStrength = parseInt( $("#Strength").val());
+    
+    var MedDelivery =parseInt( $("#Delivery").val());
+    
+    var MedQuantity =parseInt( $("#Quantity").val());
+    
+    
+    var newMed = new Med(MedName, MedType, MedStrength , MedDelivery, MedQuantity); 
+    
+    
+    console.log(newMed)
+    
+    console.log(newMed.medName)
+    OrderNew.push(newMed);
+    console.log(OrderNew)
+    
+    $("#Name").val("");
+    $("#Type").val("");
+    $("#Strength").val("");
+    $("#Delivery").val("");
+    $("#Quantity").val("");
+     
+    totalAmount = 0
+    for (let i = 0; i < OrderNew.length; i++ )
+    {
+        totalAmount += OrderNew[i].totalPrice();
+        console.log(totalAmount)
+    }
+    
+    $("#ordersTaken").append(
+        "<tr>" +
+        '<td scope="orderCalculation">' +
+       newMed.medName +
+        "</td>" +
+        "<td>" +
+        newMed.medT () +
+        " @ " +
+        newMed.medType + 
+        "</td>" +
+        "<td>" +
+        newMed.medS () +
+        " @ " +
+        newMed.medStrength + 
+        "</td>" +
+        "<td>" +
+        newMed.medD() +
+        " @ " +
+        newMed.medDelivery + 
+        "</td>" +
+        "<td>" +
+        newMed.MedQuantity +
+        "</td>" +
+        "<td>" +
+        newMed.totalPrice() +
+        "</td>" +
+        "</tr>"
+
+    );
+    
+    $("#ordersTaken").append("");
+    if (OrderNew.length > 0) {
+        $("#form-heading").empty();
+        $("#form-heading").append("Make A New Order");
+    }
+    $("#totalAmount").fadeIn();
+    $("#Checkout").fadeIn();
+    $("#orderPut").fadeIn();
+    $("#totalAmount").empty();
+    $("#totalAmount").append(totalAmount);
+    $("#totalAmount").show();
+});
+$("#Checkout").click(function () {
+$(".checkout-info").show();
+});
+$("#checkoutForm").submit(function (event) {
 event.preventDefault();
-var number1 = parseInt($("#add1").val());
-var number2 = parseInt($("#add2").val());
-var result = add(number1, number2);
-$("#output").text(result);
+var name = $("#name").val();
+var deliveryOption = $("#OptionDelivery").val();
+NameOfCustomer = name;
+$("#name").val("");
+$("#OptionDelivery").val("");
+$(".checkout-info").hide();
+$("#Checkout").hide();
+$("#totalAmount").empty();
+if (deliveryOption === "deliver") {
+    $(".deliveryLocation").show();
+    $(".deliveryCost").show();
+    $("#totalAmount").empty();
+    $("#delivery-cost").append(150);
+    totalAmount += 150;
+    $("#totalAmount").empty();
+    $("#totalAmount").empty();
+$("#totalAmount").append(totalAmount);
+$(".TotalAmount").show();
+} else {
+    alert(+"Dear, "+ NameOfCustomer + ": Your total bill is Ksh. " + totalAmount + ".Collect your order in the next one hour." + " " + " Feel free to reach out to us anytime for questions peraining our products.");
+}
 });
+$("#locationForm").submit(function (event) {
+event.preventDefault();
+var estateEntered = $("#estate").val();
+var houseNumberEntered = $("#houseNumber").val();
+estate = estateEntered;
+houseNumber = houseNumberEntered;
+$("#Checkout").hide();
+$(".deliveryLocation").hide();
+$("#totalAmount").empty();
+$("#totalAmount").append(totalAmount);
+$(".TotalAmount").show();
+alert(+ "Dear " + NameOfCustomer + ": Your new total bill is Ksh. " + totalAmount + ". Your order will be delivered to " + estate + ", " + houseNumber + " in the next one hour." + " Make full payment on delivery." + "Feel free to reach out to us anytime for questions peraining our products and deliveries.");
 });
-
-var a = document.getElementById("");
-var ausgangss = a.options[a.selectedIndex].value;
-
-		
-   
 </script>
+ 
